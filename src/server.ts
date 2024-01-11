cc13940ca1a0a716d5bcca93feaf807c2e8fae31
Visinho <Elvisvisinho@rocketmail.com>
2024-01-11 11:09:54 +0100

    Worked on the server configurations-WIP

--- src/server.ts
+++ src/server.ts
@@ -18,4 +18,30 @@ mongoose
         Logging.error(error);
     });
 
-const StartServer = () => {};
+const StartServer = () => {
+    router.use((req, res, next) => {
+        Logging.info(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+        res.on('finish', () => {
+            Logging.info(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}] - Status: [${res.statusCode}]`);
+        });
+
+        next();
+    });
+
+    router.use(express.urlencoded({ extended: true }));
+    router.use(express.json());
+
+    // API Rules
+    router.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+        if (req.method == 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+            return res.status(200).json({});
+        }
+
+        next();
+    });
+};
