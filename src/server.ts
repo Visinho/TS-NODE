adf0e4451db493b68a8d4dade33b298472afab2e
Visinho <Elvisvisinho@rocketmail.com>
2024-02-20 01:23:53 +0100

    Created the route logic for the ts files

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import http from 'http';
 import mongoose from 'mongoose';
 import { config } from './config/config';
 import Logging from './library/Logging';
+import authorRoutes from './routes/Author';
 
 const router = express();
 
@@ -46,6 +47,7 @@ const StartServer = () => {
     });
 
     //** Routes */
+    router.use('/authors', authorRoutes);
 
     /** Healthcheck */
     router.get('/ping', (req, res, next) => res.status(200).json({ message: 'Pong' }));
