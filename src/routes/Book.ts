433c79714c94209b81bb66a1cfa4d47b9e30b700
Visinho <Elvisvisinho@rocketmail.com>
2024-02-21 12:01:43 +0100

    Implemented the Joi Validation

--- src/routes/Book.ts
+++ src/routes/Book.ts
@@ -1,12 +1,13 @@
 import express from 'express';
+import { ValidateSchema, Schemas } from '../middleware/ValidateSchema';
 import controller from '../controllers/Book';
 
 const router = express.Router();
 
-router.post('/create', controller.createBook);
+router.post('/create', ValidateSchema(Schemas.book.create), controller.createBook);
 router.get('/get/:authorId', controller.readBook);
 router.get('/get/', controller.readAll);
-router.patch('/update/:authorId', controller.updateBook);
+router.patch('/update/:authorId', ValidateSchema(Schemas.book.update), controller.updateBook);
 router.delete('/delete/:authorId', controller.deleteBook);
 
 export = router;
